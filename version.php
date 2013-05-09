a3da90c0c656a0aa638530c7ef324fbbeeb21dbe
Marko Vidberg <marko.vidberg@remote-learner.net>
2013-05-09 14:16:28 -0400

    HOSSUP-1916 added requires to version.php

--- version.php
+++ version.php
@@ -8,6 +8,7 @@
  */
 
 $module->version  = 2012020201;  // The current module version (Date: YYYYMMDDXX)
+$module->requires = 2012062500;
 $module->cron     = 0;           // Period for cron to check this module (secs)
 $module->component = 'mod_adobeconnect';
 $module->maturity  = MATURITY_STABLE;
