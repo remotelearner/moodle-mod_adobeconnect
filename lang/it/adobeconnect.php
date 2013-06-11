4527ec5e7c32524479c71407e6942ec75f2c8e11
Tim Gusak <tim.gusak@remote-learner.net>
2013-06-11 09:36:19 -0400

    HOSSUP-1943 Language string changes

--- lang/it/adobeconnect.php
+++ lang/it/adobeconnect.php
@@ -34,8 +34,7 @@ $string['duplicatemeetingname'] = 'Il nome di riunione &egrave; gia in uso, vi p
 $string['duplicateurl'] = 'La URL della riunione &egrave; gia in uso, vi preghiamo di modificarla';
 $string['editingfor'] = 'Editing for: $a';
 $string['email_login'] = 'Login tramite Email address';
-$string['email_login_desc'] = 'Selezionate questa opzione solo se il login al server Connect Pro &egrave; stato impostato all\'uso dell\'email address. Attenzione che modificando '.
-                              'questa opzione durante il normale utilizzo di questo modulo, si possono creare duplicati di utenti sul server Connect';
+$string['email_login_desc'] = 'Selezionate questa opzione solo se il login al server Connect Pro &egrave; stato impostato all\'uso dell\'email address. Attenzione che modificando questa opzione durante il normale utilizzo di questo modulo, si possono creare duplicati di utenti sul server Connect';
 $string['endtime'] = 'Termine';
 $string['existingusers'] = '$a existing users';
 $string['groupswitch'] = 'Filtra per gruppo';
@@ -76,10 +75,7 @@ $string['starttime'] = 'Inizio';
 $string['usergrouprequired'] = 'Questa Riunione righiede che gli utenti siano inseriti in un gruppo per potersi collegare';
 $string['testconnection'] = 'Test Connessione';
 $string['connectiontesttitle'] = 'Test Connessione Adobe Connect';
-$string['conntestintro'] = '<p>Saranno condotti una serie di test al fine di verificare la corretta impostazione del modulo'.
-' e per determinare se le credenziali fornite hanno diritti sufficienti sul server Adobe connect per garantire il funzionamento della attivita.'.
-' Se uno dei test dovesse fallire, si rende necessario operare per risolverli, o altrimenti il modulo non funzioner&agrave;a</p><p> Per ulteriori informazioni e assistenza su come impostare il server'.
-' Adobe Connect Pro, &egrave; possibile consultare le pagine presenti su Moodledocs per questo modulo <a href="{$a->url}">Help page</a></p>';
+$string['conntestintro'] = '<p>Saranno condotti una serie di test al fine di verificare la corretta impostazione del modulo e per determinare se le credenziali fornite hanno diritti sufficienti sul server Adobe connect per garantire il funzionamento della attivita. Se uno dei test dovesse fallire, si rende necessario operare per risolverli, o altrimenti il modulo non funzioner&agrave;a</p><p> Per ulteriori informazioni e assistenza su come impostare il server Adobe Connect Pro, &egrave; possibile consultare le pagine presenti su Moodledocs per questo modulo <a href="{$a->url}">Help page</a></p>';
 $string['greaterstarttime'] = 'L\'orario di inizio non puo essere maggiore di quello di fine';
 $string['invalidadobemeeturl'] = 'Il valore inserito non e corretto.  Per maggiori informazioni potete selezionare il pulsante di help relativo a questo campo';
 
