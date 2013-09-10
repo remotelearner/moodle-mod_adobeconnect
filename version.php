2dd5db231c9eed022fce32b3818adb90abe2b810
Tyler Bannister <tyler.bannister@remote-learner.net>
2013-09-10 11:58:34 -0400

    HOSSUP-2752 - Updated release to 2.5.1.2

--- version.php
+++ version.php
@@ -5,9 +5,9 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$module->version   = 2013062701;
+$module->version   = 2013062702;
 $module->requires  = 2013051400;
 $module->cron      = 0;
 $module->component = 'mod_adobeconnect';
 $module->maturity  = MATURITY_STABLE;
-$module->release   = '2.5.1.1';
+$module->release   = '2.5.1.2';
