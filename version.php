55cb5bc5cfd10c52da7d40a791d1b7916578fb42
Linda Vanderbaan <linda.vanderbaan@remote-learner.net>
2015-06-17 11:47:06 -0400

    HOSSUP-6011 Updated to 2.9.0.0

--- version.php
+++ version.php
@@ -5,9 +5,9 @@
  * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->version   = 2014101000;
-$plugin->requires  = 2014101000;
+$plugin->version   = 2015061700;
+$plugin->requires  = 2015051100;
 $plugin->cron      = 0;
 $plugin->component = 'mod_adobeconnect';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '2.8.0.0';
+$plugin->release   = '2.9.0.0';
