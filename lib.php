9dd4afc709b35f670c1ba9bb585c90c7cac31c6f
Akbar Ahmadi <ahmadi.akbar.1996@gmail.com>
2020-06-07 14:55:07 +0430

    add require accesslib.php, remove junk code

--- lib.php
+++ lib.php
@@ -40,14 +40,8 @@ require_once('locallib.php');
 
 $adobeconnect_EXAMPLE_CONSTANT = 42;
 
-/** Include eventslib.php */
-//  require_once($CFG->libdir.'/eventslib.php');
-/** 
- * 	replaced with 'deprecatedlib.php'
- *  because eventslib.php deleted since moodle 35
- */
-
-require_once($CFG->libdir.'/deprecatedlib.php');
+/** Include accesslib.php */
+require_once($CFG->libdir.'/accesslib.php');
 /** Include calendar/lib.php */
 require_once($CFG->dirroot.'/calendar/lib.php');
 
